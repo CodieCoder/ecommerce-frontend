5958f886ebf32993a2c0e843ff5951c18a367910
CodieCoder <nonso4emma@yahoo.com>
2023-12-23 09:13:11 +0200

    fixed creat card overlapping cards

--- src/app/dashboard/cards/create/page.tsx
+++ src/app/dashboard/cards/create/page.tsx
@@ -9,15 +9,15 @@ export const metadata: Metadata = {
 
 const CreateCardPage = () => {
   return (
-    <div className="w-full h-[52.5rem]">
-      <div className="grid grid-cols-5 h-full">
-        <div className="col-span-3 px-3">
-          <div className="h-full m-auto">
+    <div className='w-full h-[52.5rem]'>
+      <div className='grid grid-cols-7 h-full'>
+        <div className='col-span-5 px-3 '>
+          <div className='h-full m-auto'>
             <CreateCardPane />
           </div>
         </div>
-        <div className="col-span-2 p-3">
-          <div className="h-[40rem] w-[70%] m-auto">
+        <div className='col-span-2 p-3'>
+          <div className='h-[40rem] m-auto'>
             <PreviewCardPane />
           </div>
         </div>
--- src/app/dashboard/index.tsx
+++ src/app/dashboard/index.tsx
@@ -12,28 +12,28 @@ const DashboardPages: React.FC<{ children: React.ReactNode }> = ({
     React.useState<DashboardSideMenuEnums>();
 
   return (
-    <div className="bg-gray-50 w-full h-screen min-w-[80rem]">
-      <div className="w-full pt-[0.5rem] pr-[5rem] bg-white grid grid-cols-6 gap-4 h-[5%] min-h-[3rem] overflow-hidden">
-        <div className="col-span-1">
-          <div className="text-center mx-auto font-bold text-xl text-sky-600">
+    <div className='bg-gray-50 w-full h-screen min-w-[80rem]'>
+      <div className='w-full pt-[0.5rem] pr-[5rem] bg-white grid grid-cols-6 gap-4 h-[5%] min-h-[3rem] overflow-hidden'>
+        <div className='col-span-1'>
+          <div className='text-center mx-auto font-bold text-xl text-sky-600'>
             App-creator
           </div>
         </div>
-        <div className="col-span-5 bg-white">
+        <div className='col-span-5 bg-white'>
           <DashboardTopBar currentDashboardPage={currentDashboardPage} />
         </div>
       </div>
-      <div className="grid grid-cols-6 h-[94%] bg-gray-100 px-[1.5rem] py-[1rem]">
-        <div className="col-span-1">
-          <aside className="flex-shrink-0 bg-white rounded-[10px] mx-auto h-[97%]">
+      <div className='grid grid-cols-6 h-[94%] bg-gray-100 px-[1.5rem] py-[1rem]'>
+        <div className='col-span-1'>
+          <aside className='flex-shrink-0 bg-white rounded-[10px] mx-auto h-[97%]'>
             {/* <aside className="flex-shrink-0 hidden w-64 bg-white border-r dark:border-primary-darker dark:bg-darker md:block"> */}
             <DashboardSidebarNav
               setCurrentDashboardPage={setCurrentDashboardPage}
             />
           </aside>
         </div>
-        <div className="col-span-5">
-          <div className="w-[90%] mx-auto border">{children}</div>
+        <div className='col-span-5'>
+          <div className='w-full mx-auto border'>{children}</div>
         </div>
       </div>
     </div>
